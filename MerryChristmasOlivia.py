70134ea3a45866696ef7b5c0e841e084028433cb
Ionut-Alberto Botoroga <230036@buas.nl>
2023-12-23 02:09:48 +0200

    .

--- MerryChristmasOlivia.py
+++ MerryChristmasOlivia.py
@@ -1,25 +1,6 @@
 import streamlit as st
 from streamlit_extras.switch_page_button import switch_page
 
-def hints():
-    if st.button('Hint 1'):
-        st.markdown('Poate poti sa aflii ceva daca citesti ce scrie pe statueta?')
-        if st.button('Hint 2'):
-            st.markdown('Poate are de a face cu numarul de pe statuie?')
-            if st.button('Hint 3'):
-                st.button('Poate numarul de pe statuie reprezinta numarul paginii?')
-
-def lvl3_function():
-    st.subheader('Level 3')
-    st.markdown('Acum pentru a completa acest nivel, va trebui sa gasesti numele altei persoane. Pentru a gasi numele persoanei potrivite va trebui sa cauti canonul lui Sherlock Holmes .pdf online. Mai departe va trebui sa utilizezi indiciile pe care le-ai descoperit pana acum. Daca ai nevoie de un sfat, sunt aici :smile:.')
-    name1 = st.text_input('Name of the person from the book')
-    if st.button('Confirm'):
-        if name1 == 'Miss Stoner':
-            return name1
-        else:
-            st.markdown('Gresit!')
-
-
 def calculate(num1, num2, operation):
     if operation == 'Add':
         result = num1 + num2
--- level3.py
+++ level3.py
@@ -1,1 +1,18 @@
 import streamlit as st
+
+if st.button('Hint 1'):
+    st.markdown('Poate poti sa aflii ceva daca citesti ce scrie pe statueta?')
+    if st.button('Hint 2'):
+        st.markdown('Poate are de a face cu numarul de pe statuie?')
+        if st.button('Hint 3'):
+            st.button('Poate numarul de pe statuie reprezinta numarul paginii?')
+
+
+st.subheader('Level 3')
+st.markdown('Acum pentru a completa acest nivel, va trebui sa gasesti numele altei persoane. Pentru a gasi numele persoanei potrivite va trebui sa cauti canonul lui Sherlock Holmes .pdf online. Mai departe va trebui sa utilizezi indiciile pe care le-ai descoperit pana acum. Daca ai nevoie de un sfat, sunt aici :smile:.')
+name1 = st.text_input('Name of the person from the book')
+if st.button('Confirm'):
+    if name1 == 'Miss Stoner':
+        st.markdown('Bravo!')
+    else:
+        st.markdown('Gresit!')
