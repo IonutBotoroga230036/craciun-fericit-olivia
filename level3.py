ba0ea957c1c5f6bb35aefe9409dd21fd19674bee
Ionut-Alberto Botoroga <230036@buas.nl>
2023-12-23 02:39:23 +0200

    Update level3.py

--- level3.py
+++ level3.py
@@ -1,8 +1,6 @@
 import streamlit as st
 
-
-
-st.subheader('Level 3')
+st.title('Level 3')
 st.markdown('Acum pentru a completa acest nivel, va trebui sa gasesti numele altei persoane. Pentru a gasi numele persoanei potrivite va trebui sa cauti canonul lui Sherlock Holmes .pdf online. Mai departe va trebui sa utilizezi indiciile pe care le-ai descoperit pana acum. Daca ai nevoie de un sfat, sunt aici :smile:.')
 name1 = st.text_input('Name of the person from the book')
 if st.button('Confirm'):
@@ -13,7 +11,7 @@ if st.button('Confirm'):
         st.markdown('Gresit!')
 if st.button('Hint 1'):
     st.markdown('Poate poti sa aflii ceva daca citesti ce scrie pe statueta?')
-    if st.button('Hint 2'):
-        st.markdown('Poate are de a face cu numarul de pe statuie?')
-        if st.button('Hint 3'):
-            st.button('Poate numarul de pe statuie reprezinta numarul paginii?')
+if st.button('Hint 2'):
+    st.markdown('Poate are de a face cu numarul de pe statuie?')
+if st.button('Hint 3'):
+    st.button('Poate numarul de pe statuie reprezinta numarul paginii?')
